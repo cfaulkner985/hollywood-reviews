8863074679718d4d93c7cf0e62b57c1ee65f0b86
cfaulkner985 <cfaulkner985@gmail.com>
2020-07-22 10:07:00 +0000

    Updated py file to include user and movie html pages

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'movie_reviews'
+app.config["MONGO_URI"] = 'mongodb+srv://cfaulkner985:mongoDB123@myfirstcluster.kyuch.mongodb.net/movie_reviews?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/add_review')
+def add_review():
+    return render_template("user.html", user_information=mongo.db.user_information.find())
+
+
+@app.route('/get_movies')
+def get_movies():
+    return render_template("movies.html", movie_information=mongo.db.movie_information.find())
 
 
 if __name__ == '__main__':
